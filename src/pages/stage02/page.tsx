5f690e824a90743d2f508d11b766d149973bf469
conao3 <conao3@gmail.com>
2025-10-19 18:07:24 +0900

    add stage1, stage2

--- src/pages/stage02/page.tsx
+++ src/pages/stage02/page.tsx
@@ -1,11 +1,3 @@
-import { Link } from 'react-router-dom';
-
 export default function Stage02() {
-  return (
-    <div>
-      <h1>Stage 02</h1>
-      <p>This is Stage 02</p>
-      <Link to="/">Back to Home</Link>
-    </div>
-  );
+  return <div className="m-0">ぷよぷよプログラミングの世界へようこそ！</div>;
 }
