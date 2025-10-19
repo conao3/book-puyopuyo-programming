f448ff2d00d4928d31d0e178df1bd18e9dedfcf3
conao3 <conao3@gmail.com>
2025-10-19 23:31:40 +0900

    edit for stage4

--- src/pages/stage04/page.tsx
+++ src/pages/stage04/page.tsx
@@ -1,6 +1,6 @@
 import Stage from './stage';
 
-export default function Stage03() {
+export default function Stage04() {
   return (
     <div className="m-0">
       <Stage />
--- src/pages/stage04/stage.tsx
+++ src/pages/stage04/stage.tsx
@@ -1,6 +1,30 @@
 import Config from './config';
 
 export default function Stage() {
+  const puyos = Array.from({ length: Config.stageRows }, (_, y) => y)
+    .flatMap((y) =>
+      Array.from({ length: Config.stageCols }, (_, x) => ({
+        x,
+        y,
+        color: Config.initialBoard[y][x],
+      })),
+    )
+    .filter(({ color }) => color >= 1 && color <= Config.puyoColorMax)
+    .map(({ x, y, color }) => (
+      <img
+        key={`${y}-${x}`}
+        src={`/img_sega/puyo_${color}.png`}
+        alt=""
+        className="absolute"
+        style={{
+          left: `${x * Config.puyoImageWidth}px`,
+          top: `${y * Config.puyoImageHeight}px`,
+          width: `${Config.puyoImageWidth}px`,
+          height: `${Config.puyoImageHeight}px`,
+        }}
+      />
+    ));
+
   return (
     <div
       className="relative mx-auto overflow-hidden"
@@ -10,6 +34,8 @@ export default function Stage() {
         backgroundColor: Config.stageBackgroundColor,
         backgroundImage: 'url(/img_sega/puyo_4bg.png)',
       }}
-    ></div>
+    >
+      {puyos}
+    </div>
   );
 }
